05d0f527ac11053a6a18b250968e7f5050249dd3
Xobaib Mahmoodi <xobaib2001.m@gmail.com>
2023-02-21 11:31:03 +0330

    modifing the name of HeaderCardButton component to HeaderCartButton

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import HeaderCardButton from "./HeaderCardButton";
+import HeaderCartButton from "./HeaderCartButton";
 import styles from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpg"; // importing an image for first time
 
@@ -8,7 +8,7 @@ const Header = (props) => {
     <Fragment>
       <header className={styles.header}>
         <h1>ReactMeals</h1>
-        <HeaderCardButton onShowCart={props.onShowCart} />
+        <HeaderCartButton onShowCart={props.onShowCart} />
       </header>
       <div className={styles["main-image"]}>
         <img src={mealsImage} alt="A tabel full of delicious foods!" />
